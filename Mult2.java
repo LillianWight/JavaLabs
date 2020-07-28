f4a8bb3e387aee8aa6999d9d3e7d60834db15fd4
LillianWight <lillian.wight@gmail.com>
2020-07-28 14:58:50 -0600

    First commit to start repo

---
+++ Mult2.java
@@ -0,0 +1,12 @@
+package labs;
+
+import java.util.Scanner;
+
+public class Mult2 {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        
+        
+    }
+}
