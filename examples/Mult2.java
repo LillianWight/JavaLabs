d812f623a375df6ceea75b852556bb6d02041cad
LillianWight <lillian.wight@gmail.com>
2020-07-28 16:00:25 -0600

    Completed program

---
+++ examples/Mult2.java
@@ -0,0 +1,14 @@
+package labs;
+
+import java.util.Scanner;
+
+public class Mult2 {
+    public static void main(final String[] args) {
+        int num = 0;
+
+        for (num = 1; num <= 16; num + 1) {
+            System.out.print((num * 2) + "\t");
+            System.out.println("");
+        }
+    }
+}
